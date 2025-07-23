e11fdb94ecb6c3d78355e702f4a96b239cf78a1f
Vitor Alves <104380373+Vitorhazael@users.noreply.github.com>
2025-07-22 21:01:33 -0300

    Portfolio_Vitor_Hazael_TI.pdf
    portfolio basico comparacoes entre numeros, ok

---
+++ Projetov4/Projetov4.py
@@ -0,0 +1,12 @@
+primeiro_valor = input('Digite o primeiro valor:')
+segundo_valor = input('Digite o segundo valor:')
+
+if primeiro_valor > segundo_valor:
+ print(f'{primeiro_valor=} e maior '
+      f'do que {segundo_valor=}' )
+
+else:
+ print(f'{primeiro_valor=} e menor '
+      f'do que {segundo_valor=}' )
+
+  # AQUI VOCE DIGITA DOIS NUMEROS ELE VAI COMPARA E DIZER SE E MAIOR OU MENOR, ETC.
